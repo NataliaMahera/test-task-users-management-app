61d3832d762ff2ca14d77866758e131b5ca253ea
NataliaMahera <magera.nataliia@gmail.com>
2024-09-14 19:08:34 +0200

    install: prittier

--- eslint.config.js
+++ eslint.config.js
@@ -16,13 +16,19 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'prettier': prettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+           { allowConstantExport: true },
       ],
     },
+    rules:
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
   },
 )
